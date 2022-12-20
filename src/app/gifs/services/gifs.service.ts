f78181f37969f1fc83003170e323763558b50ffa
ManuelQuiroga03 <111660765+ManuelQuiroga03@users.noreply.github.com>
2022-12-19 23:00:33 -0600

    Consumo de API y mostrar resultados

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-
+  private apikey: string = '3n4Eb1j2L4rZ1i0AlUxIUAhImQY4xxkO';
   private _historial: string[] = [];
+
+  public resultados : Gif[] = [];
 
   get historial(){
     return [...this._historial];
   }
 
-  buscarGifs(query: string){
-    this._historial.unshift(query);
+  constructor(private http: HttpClient){}
 
-    console.log(this._historial);
+  buscarGifs(query: string = ''){
+
+    query = query.trim().toLowerCase();
+
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+    }
+    
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=3n4Eb1j2L4rZ1i0AlUxIUAhImQY4xxkO&q=${query} z&limit=10`)
+          .subscribe((resp) => {
+            console.log(resp.data);
+            this.resultados = resp.data;
+          })
     
   }
 }
